44ecefc92babc46c6e240ea2d8adbf652760eb9c
oliveira-andre <desmonplays@gmail.com>
2019-12-07 19:12:45 -0400

    (new feature) loading products as a before action, oneline index, new start a product and the create try to create and the valid condition return if load products or return errors

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,26 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
+  before_action :load_products
+
   def index; end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    @product.valid? ? load_products : @error = true
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+
+  def load_products
+    @products = Product.all
+  end
 end
