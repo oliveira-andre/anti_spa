b62c87db12e7e8e34c8f416219adb035f08ee882
oliveira-andre <desmonplays@gmail.com>
2019-12-07 16:47:50 -0400

    (new feature) setting variant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :set_variant
+
+  private
+
+  def set_variant
+    request.variant = browser.device.mobile? ? :mobile : :desktop
+  end
 end
