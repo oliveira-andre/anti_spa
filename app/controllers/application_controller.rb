d170f322f9e74ab68ac376b70cbfe24b611c1873
oliveira-andre <desmonplays@gmail.com>
2019-12-09 23:21:22 -0400

    (new feature) first or create a cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   include FooterButtons
-  before_action :footer_buttons, :set_variant
+  before_action :footer_buttons, :set_variant, :load_cart
 
   def footer_buttons
     @footer_buttons = buttons
@@ -12,5 +12,9 @@ class ApplicationController < ActionController::Base
 
   def set_variant
     request.variant = browser.device.mobile? ? :mobile : :desktop
+  end
+
+  def load_cart
+    @cart = Cart.first_or_create(user: User.first)
   end
 end
